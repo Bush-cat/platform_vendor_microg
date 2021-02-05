02ace837ee071555626fc4606b14f5d4b8f71cb9
Bushcat <enzo.exel@gmail.com>
2021-02-05 10:31:50 +0100

    added else

--- Android.mk
+++ Android.mk
@@ -1,3 +1,23 @@
+#
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+############################################################
+# Internal build rules for APPS prebuilt modules
+############################################################
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_SHIPS_GAPPS),false)
@@ -59,31 +79,32 @@ ifeq ($(TARGET_SHIPS_GAPPS),false)
         LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
         LOCAL_PRODUCT_MODULE := true
         include $(BUILD_PREBUILT)
-    endif
-    ifeq ($(TARGET_ARCH),arm)
-        include $(CLEAR_VARS)
-        LOCAL_MODULE := BromiteWebView
-        LOCAL_MODULE_TAGS := optional
-        LOCAL_MODULE_PATH := $(TARGET_OUT)/app
-        LOCAL_SRC_FILES := system/app/BromiteWebView_arm/BromiteWebView_arm.apk
-        LOCAL_CERTIFICATE := PRESIGNED
-        LOCAL_MODULE_CLASS := APPS
-        LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-        LOCAL_OVERRIDES_PACKAGES := webview
-        LOCAL_PRODUCT_MODULE := true
-        include $(BUILD_PREBUILT)
+    else
+        ifeq ($(TARGET_ARCH),arm)
+            include $(CLEAR_VARS)
+            LOCAL_MODULE := BromiteWebView
+            LOCAL_MODULE_TAGS := optional
+            LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+            LOCAL_SRC_FILES := system/app/BromiteWebView_arm/BromiteWebView_arm.apk
+            LOCAL_CERTIFICATE := PRESIGNED
+            LOCAL_MODULE_CLASS := APPS
+            LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+            LOCAL_OVERRIDES_PACKAGES := webview
+            LOCAL_PRODUCT_MODULE := true
+            include $(BUILD_PREBUILT)
         
-        include $(CLEAR_VARS)
-        LOCAL_MODULE := Bromite
-        LOCAL_MODULE_TAGS := optional
-        LOCAL_MODULE_PATH := $(TARGET_OUT)/app
-        LOCAL_SRC_FILES := arm_ChromePublic/arm_ChromePublic.apk
-        LOCAL_CERTIFICATE := PRESIGNED
-        LOCAL_MODULE_CLASS := APPS
-        LOCAL_OVERRIDES_PACKAGES := Jelly
-        LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-        LOCAL_PRODUCT_MODULE := true
-        include $(BUILD_PREBUILT)
+            include $(CLEAR_VARS)
+            LOCAL_MODULE := Bromite
+            LOCAL_MODULE_TAGS := optional
+            LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+            LOCAL_SRC_FILES := arm_ChromePublic/arm_ChromePublic.apk
+            LOCAL_CERTIFICATE := PRESIGNED
+            LOCAL_MODULE_CLASS := APPS
+            LOCAL_OVERRIDES_PACKAGES := Jelly
+            LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+            LOCAL_PRODUCT_MODULE := true
+            include $(BUILD_PREBUILT)
+        endif
     endif
 
     include $(CLEAR_VARS)
