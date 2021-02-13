cd5a5730a6f97c3241ca60dffc65c10be5e9601d
Bushcat <73399885+Bush-cat@users.noreply.github.com>
2021-02-13 20:09:35 +0100

    fix missing separator

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 LOCAL_DEVICE := $(lastword $(subst _, ,$(TARGET_PRODUCT)))
 
-# app
+# App
 PRODUCT_PACKAGES += \
     AppleNLPBackend \
     AuroraDroid \
@@ -15,44 +15,44 @@ PRODUCT_PACKAGES += \
     LocalWiFiNLPBackend \
     MicroGGSFProxy \
     MozillaUnifiedNLPBackend \
-    NominatimNLPBackend 
+    NominatimNLPBackend
 
-# priv-app
+# Priv-app
 PRODUCT_PACKAGES += \
     AuroraServices \
     FakeStore \
     FDroidPrivileged \
-    GoogleBackupTransport \	
+    GoogleBackupTransport \
     MicroGGMSCore
 
 # Framework
 PRODUCT_PACKAGES += \
     com.google.android.maps
-
+	
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/system/overlay/
 
-# bin
+# Bin
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/bin/npem:$(TARGET_COPY_OUT_SYSTEM)/bin/npem
-
-# permissions
+	
+# Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/com.google.android.backuptransport.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.backuptransport.xml \
     $(LOCAL_PATH)/system/etc/permissions/com.google.android.media.effects.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.media.effects.xml \
     $(LOCAL_PATH)/system/etc/permissions/com.google.widevine.software.drm.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.widevine.software.drm.xml \
     $(LOCAL_PATH)/system/etc/permissions/gsync.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/gsync.xml
-
-# default-permissions
+	
+# Default-permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/default-permissions/google-sync-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/google-sync-permissions.xml \
     $(LOCAL_PATH)/system/etc/default-permissions/microg-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/microg-permissions.xml
-
-# sysconfig
+	
+# Sysconfig
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/sysconfig/nogoolag.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/nogoolag.xml
-
-# framework
+	
+# Framework
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/framework/com.google.android.maps.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/com.google.android.maps.jar
