3505d3d4bac41d39f0667e3149ab5b9d62e4f93f
Bushcat <enzo.exel@gmail.com>
2021-02-14 22:56:59 +0100

    fix cannot assign to readonly variable

--- app/Android.mk
+++ app/Android.mk
@@ -244,5 +244,3 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
 include $(BUILD_PREBUILT)
-
-$(call inherit-product, vendor/microg/config.mk)
